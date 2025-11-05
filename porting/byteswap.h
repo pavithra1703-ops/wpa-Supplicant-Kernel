f100b0977079051da0119a04ff286865a5896deb
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-05 16:52:53 +0530

    Fixing 'implicit declaration of function bswap_32' error
    - Creating header file 'porting/byteswap.h' defining bswap_32() for
      byte-swapping 32-bit integers in kernel-space builds.
    - Included this header from 'porting/endian.h' to make it globally
      available.

--- porting/byteswap.h
+++ porting/byteswap.h
@@ -1,4 +1,17 @@
 #ifndef __BYTESWAP_H_
 #define __BYTESWAP_H_
 
+#include <linux/types.h>
+
+/* Minimal kernel-compatible byte swap helpers */
+
+static inline __u32 bswap_32(__u32 x)
+{
+    return ((x & 0x000000FFU) << 24) |
+           ((x & 0x0000FF00U) << 8)  |
+           ((x & 0x00FF0000U) >> 8)  |
+           ((x & 0xFF000000U) >> 24);
+}
+
+
 #endif 
