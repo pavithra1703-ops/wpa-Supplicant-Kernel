149438398bf0d0afa295be905a8608fdb1a7bc91
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-05 12:41:10 +0530

    fixing size_t error

--- porting/stddef.h
+++ porting/stddef.h
@@ -1,4 +1,16 @@
 #ifndef __STDDEF_H_
 #define __STDDEF_H_
 
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
+
+#ifndef __SIZE_TYPE__
+#define __SIZE_TYPE__ long unsigned int
+#endif
+
+typedef __SIZE_TYPE__ size_t;
+
+typedef unsigned long size_t;
+
 #endif 
