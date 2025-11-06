ee6fd9c71f43ec885f3670d76ba98c033f91d001
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-06 13:18:24 +0530

    Fixing  implicit declaration of function ‘sleep’ and ‘usleep’ error
    - Added porting/time.h with ssleep/udelay wrappers
    - Resolves implicit declaration errors

--- porting/time.h
+++ porting/time.h
@@ -1,4 +1,17 @@
 #ifndef __TIME_H_
 #define __TIME_H_
 
+#include <linux/delay.h>
+
+/* seconds -> kernel sleep */
+static inline void sleep(unsigned int sec)
+{
+    ssleep(sec);
+}
+
+static inline void usleep(unsigned int usec)
+{
+    udelay(usec);
+}
+
 #endif 
