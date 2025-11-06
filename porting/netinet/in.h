92c05d3f92b7419da9495b2ef4fd0a00590f510e
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-06 11:31:20 +0530

    Fixing  implicit declaration of function ‘inet_ntoa’ error

--- porting/netinet/in.h
+++ porting/netinet/in.h
@@ -13,6 +13,14 @@ struct in6_addr {
     unsigned char s6_addr[16];
 };
 
+static inline char *inet_ntoa(struct in_addr in)
+{
+    static char buf[16];
+    u8 *p = (u8 *)&in.s_addr;
+    snprintf(buf, sizeof(buf), "%u.%u.%u.%u", p[0], p[1], p[2], p[3]);
+    return buf;
+}
+
 #define AF_INET 2
 #define AF_INET6 10
 
