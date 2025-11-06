a5913e659c3401e4909e1155bcff538781afbe20
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-06 16:04:53 +0530

    Fixing implicit declaration of function  ‘daemon’ error

--- porting/unistd.h
+++ porting/unistd.h
@@ -1,4 +1,12 @@
 #ifndef __UNISTD_H_
 #define __UNISTD_H_
 
+/* Stub for daemon() — kernel-space no-op */
+static inline int daemon(int nochdir, int noclose)
+{
+    /* In kernel-space, there’s no concept of daemonizing — just ignore */
+    return 0;
+}
+
+
 #endif 
