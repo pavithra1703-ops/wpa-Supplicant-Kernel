7063362b2773a35c62f5310329ad887002fe31ae
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-06 18:04:30 +0530

    Fixing ‘SEEK_END’ and ‘SEEK_SET’ undeclared errors

--- porting/stdio.h
+++ porting/stdio.h
@@ -3,6 +3,12 @@
 
 typedef void  FILE;
 
+
+/* File position macros */
+#define SEEK_SET 0
+#define SEEK_CUR 1
+#define SEEK_END 2
+
 #include <linux/printk.h>
 
 /* Stub fread() for kernel space */
