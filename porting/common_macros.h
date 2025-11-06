f22368dfe509f9ec5a42820167d0ea8312cea008
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-06 16:07:24 +0530

    Fixing implicit declaration of function ‘perror’ error

--- porting/common_macros.h
+++ porting/common_macros.h
@@ -1,6 +1,9 @@
 #ifndef __COMMON_MACROS_H_
 #define __COMMON_MACROS_H_
 
+/* Stub for perror() — replace with kernel log */
+#include <linux/printk.h>   // for printk()
+
 /* Undefine any previous ARRAY_SIZE macro to avoid redefinition warning */
 #ifdef ARRAY_SIZE
 #undef ARRAY_SIZE
@@ -8,6 +11,14 @@
 
 /* Define ARRAY_SIZE macro */
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+
+
+static inline void perror(const char *s)
+{
+    /* Kernel doesn't have stderr — print to kernel log instead */
+    printk(KERN_ERR "perror: %s\n", s ? s : "(null)");
+}
+
 
 #endif /* __COMMON_MACROS_H_ */
 
