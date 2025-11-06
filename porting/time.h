21d74b8c8967ba64b4fe0c219bc2780019956d39
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-06 13:34:40 +0530

    Fixing implicit declaration of function ‘gettimeofday’ error

--- porting/time.h
+++ porting/time.h
@@ -2,6 +2,8 @@
 #define __TIME_H_
 
 #include <linux/delay.h>
+#include <linux/timekeeping.h>  // for ktime_get_real_ts64
+#include "timeval.h"             // your kernel-space timeval
 
 /* seconds -> kernel sleep */
 static inline void sleep(unsigned int sec)
@@ -13,5 +15,21 @@ static inline void usleep(unsigned int usec)
 {
     udelay(usec);
 }
+
+
+
+/* Kernel-space replacement for gettimeofday() */
+static inline int gettimeofday(struct timeval *tv, void *tz)
+{
+    struct timespec64 ts;
+    ktime_get_real_ts64(&ts);
+    if (tv) {
+        tv->tv_sec  = ts.tv_sec;
+        tv->tv_usec = ts.tv_nsec / 1000;
+    }
+    return 0;
+}
+
+
 
 #endif 
