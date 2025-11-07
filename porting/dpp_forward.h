89fa75db9c727e72fe71d16a3ec897073c7dadc7
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-07 15:58:58 +0530

    Fixing invalid use of undefined type ‘struct dpp_authentication’ error

--- porting/dpp_forward.h
+++ porting/dpp_forward.h
@@ -2,7 +2,11 @@
 #define __DPP_FORWARD_H_
 
 // Forward declaration of the struct
-struct dpp_authentication;
+struct dpp_authentication {
+    void *msg_ctx;  // stub the type as void*, enough for the kernel port
+    // add other fields as needed, with minimal types
+};
+
 
 // Forward declaration of the function
 void dpp_auth_fail(struct dpp_authentication *auth, const char *txt);
