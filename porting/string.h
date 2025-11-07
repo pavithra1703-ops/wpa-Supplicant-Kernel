e551dee7261a5180ac2654a72d280869ba91d24c
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-07 12:26:45 +0530

    Fixing implicit declaration of function ‘strerror’ error

--- porting/string.h
+++ porting/string.h
@@ -1,4 +1,15 @@
 #ifndef __STRING_H_
 #define __STRING_H_
 
+
+#include <linux/kernel.h>  // for printk
+
+static inline const char *strerror(int errnum)
+{
+    // Kernel cannot map errno to string in same way as user-space
+    printk(KERN_INFO "strerror(%d) called in kernel-space\n", errnum);
+    return "kernel-space stub";
+}
+
+
 #endif 
