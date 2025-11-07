ecc2c05e9648089e1da09131a28e80cd4f4779b8
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-07 15:55:32 +0530

    Fixing ‘dpp_authentication’ and 'dpp_auth_fail' warnings
    - created porting/dpp_forward.h to declare dpp_authentication and dpp_auth_fail for kernel builds.
    - Included 'dpp_forward.h' in 'porting/stdlib.h' for global availability
    - Resolves warnings

---
+++ porting/dpp_forward.h
@@ -0,0 +1,11 @@
+#ifndef __DPP_FORWARD_H_
+#define __DPP_FORWARD_H_
+
+// Forward declaration of the struct
+struct dpp_authentication;
+
+// Forward declaration of the function
+void dpp_auth_fail(struct dpp_authentication *auth, const char *txt);
+
+#endif // __DPP_FORWARD_H_
+
