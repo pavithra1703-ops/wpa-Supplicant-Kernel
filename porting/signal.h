1da7904bb797d0963599098ebbcbf6c0f063fa77
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-07 12:18:10 +0530

    Fixing implicit declaration of function ‘signal’ error

--- porting/signal.h
+++ porting/signal.h
@@ -1,4 +1,21 @@
 #ifndef __SIGNAL_H_
 #define __SIGNAL_H_
 
+
+#include <linux/kernel.h>  // for printk/WARN_ON
+
+/* Signal stubs for kernel-space */
+#ifndef SIGALRM 
+#define SIGALRM 0
+#endif
+
+static inline void (*signal(int signum, void (*handler)(int)))(int)
+{
+    (void)signum;
+    (void)handler;
+    printk(KERN_INFO "stub signal() called in kernel-space, ignored\n");
+    return NULL;
+}
+
+
 #endif 
