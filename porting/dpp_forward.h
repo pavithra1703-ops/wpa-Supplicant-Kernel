71cd1ab7b97b854104ff297ae70f46bfc7acaf83
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-07 16:24:41 +0530

    Fixing no previous prototype for ‘dpp_get_attr’ and ‘dpp_check_attrs’ warnings

--- porting/dpp_forward.h
+++ porting/dpp_forward.h
@@ -1,6 +1,9 @@
 #ifndef __DPP_FORWARD_H_
 #define __DPP_FORWARD_H_
 
+#include <stddef.h>  // for size_t
+#include <stdint.h>  // for u8, u16
+
 struct dpp_authentication {
     void *msg_ctx;
 };
@@ -14,6 +17,13 @@ enum dpp_public_action_frame_type {
 void dpp_auth_fail(struct dpp_authentication *auth, const char *txt);
 struct wpabuf *dpp_alloc_msg(enum dpp_public_action_frame_type type,
                              size_t len);
+
+// Add missing prototypes to fix -Wmissing-prototypes warnings
+const uint8_t *dpp_get_attr(const uint8_t *buf, size_t len,
+                            uint16_t req_id, uint16_t *ret_len);
+
+int dpp_check_attrs(const uint8_t *buf, size_t len);
+
 
 #endif // __DPP_FORWARD_H_
 
