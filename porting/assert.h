1fbcb80a9d9779393a22730e45e12092ff6fd54d
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-07 12:09:16 +0530

    Fixing implicit declaration of function ‘assert’ error

--- porting/assert.h
+++ porting/assert.h
@@ -1,4 +1,19 @@
 #ifndef __ASSERT_H_
 #define __ASSERT_H_
 
+#include <linux/kernel.h>  /* for printk */
+
+/*
+ * Stub assert() for kernel builds
+ * In kernel space, we just print a warning if the condition fails.
+ */
+#define assert(expr) \
+    do { \
+        if (!(expr)) { \
+            printk(KERN_WARNING "Assertion failed: %s, in %s:%d\n", \
+                   #expr, __FILE__, __LINE__); \
+        } \
+    } while (0)
+
 #endif 
+
