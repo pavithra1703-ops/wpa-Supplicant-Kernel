9d45c2335a8c0864dbbfafdeb3ee5466e71a13ac
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-11 18:56:53 +0530

    Fixing ‘INET_ADDRSTRLEN’ and ‘INET6_ADDRSTRLEN’ undeclared errors

--- porting/inttypes.h
+++ porting/inttypes.h
@@ -1,4 +1,16 @@
 #ifndef __INTTYPES_H_
 #define __INTTYPES_H_
 
+
+/* Define missing address string length constants if not already defined */
+#ifndef INET_ADDRSTRLEN
+#define INET_ADDRSTRLEN 16
+#endif
+
+#ifndef INET6_ADDRSTRLEN
+#define INET6_ADDRSTRLEN 46
+#endif
+
+
+
 #endif 
