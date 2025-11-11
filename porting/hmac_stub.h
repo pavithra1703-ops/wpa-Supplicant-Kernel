496512fa4cedd1c2d1cc5a18b231d0e1a57752d8
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-11 15:28:41 +0530

    Fixing implicit declaration of function ‘hmac_sha1’ error

--- porting/hmac_stub.h
+++ porting/hmac_stub.h
@@ -13,5 +13,14 @@ static inline int hmac_md5(const u8 *key, size_t key_len,
     return 0; /* pretend success */
 }
 
+/* Stub hmac_sha1 for kernel-space */
+static inline int hmac_sha1(const u8 *key, size_t key_len,
+                            const u8 *data, size_t data_len, u8 *mac)
+{
+    if (mac)
+        memset(mac, 0, 20);  /* 20 bytes for SHA1 */
+    return 0; /* pretend success */
+}
+
 #endif /* __HMAC_STUB_H */
 
