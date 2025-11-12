d24c366ace42738d875a9cc9f22b63e052a3c4d5
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-12 11:24:06 +0530

    Fixing redefinition of ‘eapol_sm_step’ error

---
+++ porting/eapol_fix.h
@@ -0,0 +1,13 @@
+#ifndef __EAPOL_FIX_H
+#define __EAPOL_FIX_H
+
+#include "../wpa_supplicant-2.11/src/eapol_supp/eapol_supp_sm.h"
+
+/* Step 2: declare your own renamed function */
+void my_porting_eapol_sm_step(struct eapol_sm *sm);
+
+/* Step 3: redirect all further eapol_sm_step calls to your version */
+#define eapol_sm_step my_porting_eapol_sm_step
+
+#endif // PORTING_EAPOL_STEP_FIX
+
