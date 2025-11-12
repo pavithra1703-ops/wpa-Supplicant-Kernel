16e171c4065b19cb99a17bd64a36e8ab6868fb9a
pavithra1703-ops <panigala.pavithra@capgemini.com>
2025-11-12 11:28:32 +0530

    Fixing implicit declaration of function ‘eap_peer_sm_step’ and ‘eap_peer_was_failure_expected’ errors

--- porting/eapol_fix.h
+++ porting/eapol_fix.h
@@ -9,5 +9,21 @@ void my_porting_eapol_sm_step(struct eapol_sm *sm);
 /* Step 3: redirect all further eapol_sm_step calls to your version */
 #define eapol_sm_step my_porting_eapol_sm_step
 
+
+struct eap_sm;  // forward declaration
+
+// Dummy implementations
+static inline int eap_peer_sm_step(struct eap_sm *sm)
+{
+    // For porting: do nothing, return 0 (success)
+    return 0;
+}
+
+static inline int eap_peer_was_failure_expected(struct eap_sm *sm)
+{
+    // Always return false (no failure expected)
+    return 0;
+}
+
 #endif // PORTING_EAPOL_STEP_FIX
 
